ad551739ceb107a9a264da77e366929045af12a9
Victor tondolo <victor.tondolo@gmail.com>
2021-03-07 12:24:26 +0100

    Sort routes by endpoint and remove uppercases and lowercases in routes

--- routes/index.ts
+++ routes/index.ts
@@ -5,52 +5,45 @@ import { TimeSlotController } from '../src/controller/TimeSlotController';
 import { CourseController } from '../src/controller/CourseController';
 import { ExchangeController } from '../src/controller/ExchangeController';
 
-
-/**********************************************************************
-******************************* GET **********************************
-**********************************************************************/
-
 /* GET home page. */
 router.get('/', function(req?: any, res?: any, next?: any) {
   res.render('index', { title: 'Express' });
 });
 
+/*********** Route: emploidut ************/
+
 /* GET Emploidutemps. */
-router.get('/emploiDuTemps', function(req?: any, res?: any, next?: any) {
+router.get('/emploidut', function(req?: any, res?: any, next?: any) {
   res.render('index', { title: `Emploid'ut` });
 });
 
-/* GET timeSlots for one student (login) */
-router.get('/timeSlots', TimeSlotController.getTimeSlots)
+/*********** Route: timeslots ************/
+
+/* GET timeslots for one student (login) */
+router.get('/timeslots', TimeSlotController.getTimeSlots)
+
+/* POST timeslots for one student (login) */
+router.post('/timeslots', TimeSlotController.postTimeSlots)
+
+/* PUT timeslots for one student (login) */
+router.put('/timeslots', TimeSlotController.putTimeSlots)
+
+/*********** Route: courses ************/
 
 /* GET courses for one student (login) */
 router.get('/courses', CourseController.getCourses)
+/* POST courses for one student (login) */
+router.post('/courses', CourseController.postCourses)
+/* PUT courses for one student (login) */
+router.put('/courses', CourseController.putCourses)
+
+/*********** Route: exchanges ************/
 
 /* GET exchange for one student (login) */
 router.get('/exchanges', ExchangeController.getExchanges)
 
-/**********************************************************************
-******************************* POST **********************************
-**********************************************************************/
-
-/* POST timeSlots for one student (login) */
-router.post('/timeSlots', TimeSlotController.postTimeSlots)
-
-/* POST courses for one student (login) */
-router.post('/courses', CourseController.postCourses)
-
 /* POST exchange for one student (login) */
 router.post('/exchanges', ExchangeController.postExchanges)
-
-/**********************************************************************
-******************************* PUT ***********************************
-**********************************************************************/
-
-/* PUT timeSlots for one student (login) */
-router.put('/timeSlots', TimeSlotController.putTimeSlots)
-
-/* PUT courses for one student (login) */
-router.put('/courses', CourseController.putCourses)
 
 /* PUT exchange for one student (login) */
 router.put('/exchanges', ExchangeController.putExchanges)
