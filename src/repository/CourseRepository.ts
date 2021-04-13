59dd9ebdcefb0da289e964545e7f02d9cb74d503
Victor tondolo <victor.tondolo@gmail.com>
2021-04-13 23:12:32 +0200

    Add package.json

--- src/repository/CourseRepository.ts
+++ src/repository/CourseRepository.ts
@@ -2,4 +2,4 @@ import {EntityRepository, Repository} from "typeorm";
 import {Course} from "../entity/Course";
 
 @EntityRepository(Course)
-class CourseRepository extends Repository<Course> {}
+export class CourseRepository extends Repository<Course> {}
