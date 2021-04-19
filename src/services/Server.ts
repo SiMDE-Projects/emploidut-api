0a111dd201f4871c9c63ff56d83d722ae06709ec
Victor tondolo <victor.tondolo@gmail.com>
2021-04-19 18:54:24 +0200

    Implements authentication in the server config

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -1,20 +1,22 @@
+var qs = require('querystring');
 require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('../../routes/index');
-var usersRouter = require('../../routes/users');
 var app = express();
 const port = process.env.PORT || 3000;
+
+import { oauth2, authURL } from '../services/Authentication';
 
 export class Server {
 
     constructor() {}
 
     async start() {
+        
         // view engine setup
         app.set('views', path.resolve('./', './views'));
         app.set('view engine', 'pug');
@@ -25,8 +27,41 @@ export class Server {
         app.use(cookieParser());
         //app.use(express.static(path.join(__dirname, './../public')));
 
+        app.all('*', function (req?: any, res?: any, next?: any) {
+            /**
+             * Check if the user is connected
+             */
+            if (req.query.code === null || req.query.code === undefined) {
+                // Handle redirection (the user is not connected th oauth2 yet)
+                return res.redirect(authURL);
+            } else {
+                /** Obtaining access_token */
+                oauth2.getOAuthAccessToken(
+                req.query.code,
+                {
+                    'redirect_uri': 'http://localhost:3000/',
+                    'grant_type':'authorization_code'
+                },
+                function (e, access_token, refresh_token, results){
+                    if (e) {
+                        console.log(e);
+                        res.end(e);
+                    } else if (results.error) {
+                        console.log(results);
+                        res.end(JSON.stringify(results));
+                    }
+                    else {
+                        console.log('Obtained access_token: ', access_token);
+                        res.end( access_token);
+                    }
+                });
+            }
+
+            // Send the request to next server's middlware
+            next();
+        });
+
         app.use('/', indexRouter);
-        app.use('/users', usersRouter);
 
         // catch 404 and forward to error handler
         app.use(function(req?: any, res?: any, next?: any) {
