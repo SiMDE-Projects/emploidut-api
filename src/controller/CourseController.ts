35566bf36d6dd39f5cdf1f877e246edba0a5a0d2
Victor tondolo <victor.tondolo@gmail.com>
2021-04-21 19:16:42 +0200

    Improve course controller

--- src/controller/CourseController.ts
+++ src/controller/CourseController.ts
@@ -7,11 +7,22 @@ export class CourseController {
      * Expect the login in queryParams
     */
     public static getCourses = async (req?: any, res?: any, next?: any) => {
-        if (req.query.uv !== undefined && req.query.uv !== null) {
-            const courses = await getRepository(Course).find({ id: req.query.uv });
-            res.json(courses);
+
+        // Check if there is a query named course
+        const courseQueryParam = req.query.course;
+        if (courseQueryParam !== undefined && courseQueryParam !== null) {
+            const course = await getRepository(Course).findOne({ id: courseQueryParam });
+            
+            if (course === undefined  || course === null) {
+                // Send 404 error
+                res.status(404).send('Entity not found').end();
+                return;
+            }
+
+            res.json(course);
             return;
         } else {
+            // Return every courses in DB
             const courses = await getRepository(Course).find();
             res.json(courses);
             return;
