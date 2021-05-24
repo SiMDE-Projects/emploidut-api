c11b7578f444b394804d924592ebc77f185c3949
Victor tondolo <victor.tondolo@gmail.com>
2021-05-24 14:42:37 +0200

    Add exchange entity

--- src/entity/Exchange.ts
+++ src/entity/Exchange.ts
@@ -1,17 +1,48 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { TimeSlot } from "./TimeSlot";
+import { User } from "./User";
+
+export type exchangeStatus = 'PENDING' | 'ACCEPTED' | 'VALIDATED';
 
 /**
- * Initial class which handles all the course exchanges
+ * Initial class which handles all the course's exchanges
  * @class Exchange
  */
 @Entity()
 export class Exchange {
+
     @PrimaryGeneratedColumn()
-    id: number = 0;
+    id?: number;
+
+    @Column({
+        type: "enum",
+        enum: ["PENDING" , "ACCEPTED" , "VALIDATED"],
+        default: "PENDING"
+    })
+    status?: exchangeStatus = 'PENDING';
+
+    @ManyToOne(() => TimeSlot, timeSlot => timeSlot.id, {
+        eager: true
+    })
+    exchangedTimeslot?: TimeSlot;
+
+    @ManyToOne(() => TimeSlot, timeSlot => timeSlot.id, {
+        eager: true
+    })
+    desiredTimeslot?: TimeSlot;
+
+    @ManyToOne(() => User, user => user.id, {
+        eager: true
+    })
+    suggesterStudent?: User;
+
+    @ManyToOne(() => User, user => user.id, {
+        eager: true
+    })
+    aimStudent?: User;
 
     /**
-     * Constructor for Course
+     * Constructor for Exchange
      */
-    constructor () {
-    }
+    constructor () {}
 }
