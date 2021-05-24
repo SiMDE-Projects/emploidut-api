1ecb390fe3460fe666b4ce07fded93776c2af387
Victor tondolo <victor.tondolo@gmail.com>
2021-05-24 22:35:59 +0200

    Get courses by id and add comments

--- src/controller/CourseController.ts
+++ src/controller/CourseController.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { getRepository } from "typeorm";
 import { Course } from "../entity/Course";
 import { CourseService } from "../services/CourseService";
 
@@ -15,47 +14,51 @@ export class CourseController {
     }
 
     public routes(){
+        this.router.get('/:id', this.findOne);
         this.router.get('/', this.getCourses);
         this.router.post('/', this.postCourses);
         this.router.put('/', this.putCourses);
     }
 
     /**
-     * GET courses for one student (login)
-     * Expect the login in queryParams
+     * GET course by id 
     */
-    public getCourses = async (req: Request, res: Response, next: NextFunction) => {
-
-        // Check if there is a query named course
-        const courseQueryParam = req.query.course;
-        if (courseQueryParam !== undefined && courseQueryParam !== null) {
-            const course = await getRepository(Course).findOne({ id: String(courseQueryParam) });
-            
+     public findOne = async (req: Request, res: Response, next: NextFunction) => {
+        const courseId = req.params.id;
+        if (courseId === undefined || courseId === null) {
+            res.status(400).send("Error, parameter id is missing or wrong");
+        }
+        else {
+            const course = await this.courseService.findOne(courseId);
             if (course === undefined  || course === null) {
                 // Send 404 error
                 res.status(404).send('Entity not found').end();
                 return;
             }
 
+            // Send course found
             res.json(course);
-            return;
-        } else {
-            // Return every courses in DB
-            const courses = await getRepository(Course).find();
-            res.json(courses);
             return;
         }
     }
 
     /**
-     * GET courses for one student (login)
-     * Expect the login in queryParams
+     * GET courses 
+    */
+    public getCourses = async (req: Request, res: Response, next: NextFunction) => {
+        // Return every courses in DB
+        const courses = await this.courseService.findAll();
+        res.json(courses);
+        return;
+    }
+
+    /**
+     * POST courses
     */
     public postCourses = async (req: Request, res: Response, next: NextFunction) => {}
 
     /**
-     * PUT courses for one student (login)
-     * Expect the login in queryParams
+     * PUT courses
     */
     public putCourses = async (req: Request, res: Response, next: NextFunction) => {}
 }
