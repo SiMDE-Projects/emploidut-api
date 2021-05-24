b7ecd3060d300f4d6eff15457077809bc14c553d
Victor tondolo <victor.tondolo@gmail.com>
2021-05-24 15:03:57 +0200

    Add controller, service and repository for exchanges

--- src/controller/ExchangeController.ts
+++ src/controller/ExchangeController.ts
@@ -1,6 +1,5 @@
 import { Request, Response, Router } from 'express';
 import { ExchangeService } from '../services/ExchangeService';
-import { Exchange } from './../entity/Exchange';
 
 export class ExchangeController {
 
@@ -14,16 +13,32 @@ export class ExchangeController {
     }
 
     public routes(){
+        this.router.get('/:id', this.findOne);
         this.router.get('/', this.getExchanges);
         this.router.post('/', this.postExchanges);
         this.router.put('/', this.putExchanges);
     }
 
     /**
+     * GET exchange by id 
+    */
+     public findOne = async (req: Request, res: Response, next?: any) => {
+        const exchangeId = req.params.id;
+        if (typeof exchangeId === undefined || exchangeId === null) {
+            res.status(400).send("Error, parameter id is missing or wrong");
+        }
+        else{
+            res.send(await this.exchangeService.findUser(parseInt(exchangeId, 10)))
+        }
+    }
+
+    /**
      * GET exchanges for one student (login) 
      * Expect the login in queryParams
     */
-    public getExchanges = async (req: Request, res: Response, next?: any) => {}
+    public getExchanges = async (req: Request, res: Response, next?: any) => {
+        res.send(await this.exchangeService.findAll());
+    }
 
     /**
      * POST exchanges for one student (login) 
