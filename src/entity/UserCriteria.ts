3f8598b15c2674a857e0d88398d0462e92d4be2e
Victor tondolo <victor.tondolo@gmail.com>
2021-05-24 12:56:11 +0200

    Add deserializer for userCriteria

--- src/entity/UserCriteria.ts
+++ src/entity/UserCriteria.ts
@@ -1,7 +1,33 @@
-export class UserCriteria{
+export class UserCriteria {
     firstName?: String | null = null;
-    lastName?: String | null = null;
+    lastName?: String| null = null;
     semester?: String | null = null;
     enableConsultation?: Boolean | null = null;
     enableViewing?: Boolean | null = null;
+
+    deserialize(object: any) {
+        const { firstName, lastName, semester, enableConsultation, enableViewing } = object;
+
+        if (firstName !== null && firstName !== undefined) {
+            this.firstName = firstName;
+        }
+
+        if (lastName !== null && lastName !== undefined) {
+            this.lastName = lastName;
+        }
+
+        if (semester !== null && semester !== undefined) {
+            this.semester = semester;
+        }
+
+        if (enableConsultation !== null && enableConsultation !== undefined) {
+            this.enableConsultation = enableConsultation;
+        }
+
+        if (enableViewing !== null && enableViewing !== undefined) {
+            this.enableViewing = enableViewing;
+        }
+
+        return this;
+    }
 }
