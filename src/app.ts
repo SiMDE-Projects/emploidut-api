a4de70391e69347f7b8f37af6149a06198c2b37f
Victor tondolo <victor.tondolo@gmail.com>
2021-07-01 21:17:10 +0200

    Use logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import Logger from "./services/Logger";
 import { Server } from './services/Server';
 
 // Cretate the connection to the database
@@ -7,12 +8,12 @@ import { Server } from './services/Server';
     // Start the app
     const server = new Server();
     server.start().then( () => {
-      console.log(`The server has started`);
+      Logger.debug(`The server has started`);
     });
 
     // Catch Ctrl+C and properly stop the app
     process.on('SIGINT', () => {
-      console.log('SIGINT (Ctrl+C) received. Stopping emploidut.')
+      Logger.debug('SIGINT (Ctrl+C) received. Stopping emploidut.')
       server.stop().then( () => {
         process.exit();
       })
