8339401c7cae82429fc00450d9dd9c837a6e1bfa
Victor tondolo <victor.tondolo@gmail.com>
2021-07-03 19:27:58 +0200

    Add first skeleton for timeTableService

--- src/controller/TimetableController.ts
+++ src/controller/TimetableController.ts
@@ -1,21 +1,23 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../entity/User";
+import Logger from "../services/Logger";
 import { TimeTableService } from "../services/TimetableService";
+import { UserService } from "../services/UserService";
 
 export class TimetableController {
 
     private timeTableService: TimeTableService;
+    private userService: UserService;
     public router: Router;
 
     constructor() {
         this.timeTableService = new TimeTableService();
+        this.userService = new UserService();
         this.router = Router();
         this.routes();
     }
 
     public routes(){
-        this.router.get('/emploidut', this.getEmploidut);
+        this.router.get('/', this.getEmploidut);
     }
 
     /**
@@ -26,21 +28,38 @@ export class TimetableController {
      * @returns 
      */
     public getEmploidut = async (req: Request, res: Response, next: NextFunction) => {
-        if (req.query.login) {
-            const user = await getRepository(User).findOne({ id: req.query.login });
-
-            if (user === null || user === undefined) {
-                res.status(404).send('Sorry user not found!').end();
+        Logger.debug('GET Emploidut');
+        const login = <String> req.query.login;
+        if (login) {
+            Logger.debug('GET Emploidut from another login');
+            const user = await this.userService.findByLogin(login);
+            if (user !== undefined) {
+                const timeTable = await this.timeTableService.findTimeTable(user);
+                res.send(timeTable).end();
+                return;
+            } else {
+                res.status(404).send('User not found').end();
                 return;
             }
-
-            // TODO: Add findEmploidut method 
-            // const emploidut = await getRepository(--).findEmploidut(req.query.login);
-            // res.json(emploidut); 
-        } else {
-            // Send error 404 error
-            res.status(404).send('Parameter is missing!').end();
-            return;
+        }
+        else {
+            Logger.debug('GET Emploidut for the requester user');
+            const requestUser = res.locals.user;
+            if (requestUser === undefined || requestUser === null) {
+                res.status(404).send('User not found').end();
+                return;
+            } else {
+                const user = await this.userService.findUser(requestUser.id);
+                if (user !== undefined) {
+                    const timeTable = await this.timeTableService.findTimeTable(user);
+                    res.send(timeTable).end();
+                    return;
+                } else {
+                    // The user is not in DB --> Store him/her and him/her timeTable (emploidut)
+                    res.end();
+                    return;
+                }
+            }
         }
     }
 }
--- src/services/TimetableService.ts
+++ src/services/TimetableService.ts
@@ -1,4 +1,49 @@
+import { getCustomRepository } from "typeorm";
+import { TimeSlotRepository } from "../repository/TimeSlotRepository";
+import axios from 'axios';
+import Logger from "./Logger";
+import { User } from "../entity/User";
+
 /**
  * TimeTable service class
 */
-export class TimeTableService {}
+export class TimeTableService {
+
+    private timeSlotRepository: TimeSlotRepository;
+
+    constructor() {
+        this.timeSlotRepository = getCustomRepository(TimeSlotRepository);
+    }
+    
+    /**
+     * Find the timeTable (emploidut) for one user
+     * @param login Login of the user
+     * @returns 
+     */
+    public findTimeTable = async (user: User) => {
+        // Try get value from UTC
+        const responseAxios = {
+            status: 404,
+            data: null,
+        };
+        // const responseAxios = await axios({
+        //     method: 'GET',
+        //     url: `${process.env.TIME_TABLE_BASE_ROUTE}`,
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Accept-Charset': 'utf-8',
+        //     }
+        // }).catch((err: any) => {
+        //     return err.response;
+        // }).then((response: any) => {
+        //     return response;
+        // });
+
+        if (responseAxios.status !== 200 || responseAxios.data !== undefined || responseAxios.data === null) {
+            // TODO: Add real function to find the timeTable (emploidut)
+            return { "timeslots": [] };
+        } else {
+            return "I need to parse the result";
+        }
+    }
+}
