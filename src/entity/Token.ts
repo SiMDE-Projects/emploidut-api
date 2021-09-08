a7ea754baa02d0e28bc686a9bee9de7afbd3fa9d
Victor tondolo <victor.tondolo@gmail.com>
2021-09-08 17:29:42 +0200

    Add Token class instead of authenticator

---
+++ src/entity/Token.ts
@@ -0,0 +1,77 @@
+import { oauth2 } from "../services/Authentication";
+import Logger from "../services/Logger";
+
+const jwt_decode = require('jwt-decode');
+
+/**
+ * Initial class for one course
+ * @class Course
+ */
+export class Token {
+
+    private static refresh_token: string = '';
+    private static access_token: string = '';
+
+    // Create a token for the client (emploidut back end)
+    static createToken = () => {
+        // Obtaining access_token for the client
+        oauth2.getOAuthAccessToken(
+            '',
+            {
+                'grant_type':'client_credentials',
+                'client_id': process.env.AUTH_CLIENT_ID,
+                'client_password': process.env.AUTH_CLIENT_PASSWORD,
+            },
+            (err:any, access_token:any, refresh_token:any, results:any) => {
+                Logger.debug('Client access_token: ' + access_token);
+                Token.refresh_token = refresh_token;
+                Token.access_token = access_token;
+                return;
+            }
+        );
+    }
+
+    // Get access_token
+    static getAccessToken = (): string => {
+        if (Token.access_token === '') {
+            Token.createToken();
+        }
+
+        return Token.access_token;
+    }
+
+    // Refresh old access_token
+    static refreshToken = () => {
+        if (Token.access_token === '' || Token.refresh_token === '') {
+            Token.createToken();
+        }
+
+        oauth2.getOAuthAccessToken(
+            '',
+            {
+                'grant_type': 'refresh_token',
+                'refresh_token': Token.refresh_token,
+                'client_id': process.env.AUTH_CLIENT_ID,
+                'client_password': process.env.AUTH_CLIENT_PASSWORD,
+            },
+            (err:any, access_token:any, refresh_token:any, results:any) => {
+                Logger.debug('Client access_token: ' + access_token);
+                Token.access_token = access_token;
+                return;
+            }
+        );
+    }
+
+    static isValid = (): boolean => {
+        if (Token.access_token === '') {
+            Token.createToken();
+        }
+
+        // Decode token
+        var decoded_token = jwt_decode(Token.access_token);
+
+        // Get today's timestamp
+        const now = Math.floor(Date.now() / 1000)
+        return decoded_token && decoded_token.exp > now;
+    }
+}
--- src/services/Authentication.ts
+++ src/services/Authentication.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { Token } from "../entity/Token";
 import Logger from "./Logger";
 
 var OAuth = require('oauth');
@@ -8,7 +9,7 @@ var axios = require('axios');
 const portailURL = process.env.AUTH_PORTAIL_URL;
 const redirectURL = process.env.AUTH_REDIRECT_URL;
 
-var oauth2 = new OAuth.OAuth2(
+export var oauth2 = new OAuth.OAuth2(
     process.env.AUTH_CLIENT_ID,
     process.env.AUTH_CLIENT_SECRET,
     `${portailURL}/`,
@@ -24,57 +25,8 @@ var authURL = oauth2.getAuthorizeUrl({
     state: ''
 });
 
-// Create a token for the client (emploidut back end)
-const createClientToken = async () => {
-    // Obtaining access_token for the client
-    oauth2.getOAuthAccessToken(
-        '',
-        {
-            'grant_type':'client_credentials',
-            'client_id': process.env.AUTH_CLIENT_ID,
-            'client_password': process.env.AUTH_CLIENT_PASSWORD,
-        },
-        async function (err:any, access_token:any, refresh_token:any, results:any) {
-            Logger.debug('Client access_token: ' + access_token);
-            process.env.CLIENT_TOKEN = JSON.stringify(results);
-            return access_token;
-        }
-    );
-}
-
-// Get a token for the client (emploidut back end)
-export const getClientToken = async () => {
-    // Check if a token exists in the environment variables
-    if (process.env.CLIENT_TOKEN === undefined || process.env.CLIENT_TOKEN === null) {
-        return await createClientToken();
-    } else {
-        // Extract token from env
-        const token = JSON.parse(process.env.CLIENT_TOKEN);
-
-        // Check token validity
-        const responseAxios = await axios({
-            method: 'GET',
-            url: `${portailURL}/api/v1/client/users`,
-            headers: {
-                'Accept': 'application/json',
-                'Accept-Charset': 'utf-8',
-                'Authorization': 'Bearer ' + token.access_token,
-            }
-        }).catch((err: any) => {
-            return err.response;
-        }).then((response: any) => {
-            return response;
-        });
-
-        if (responseAxios.status === 200) {
-            return token.access_token;
-        } else {
-            return await createClientToken();
-        }
-    }
-}
-
 export const authenticationFilter = async function (req: Request, res: Response, next: NextFunction) {
+    Logger.debug('Access Token: ' + Token.getAccessToken());
     // Check if the request contains a valid token
     let token = req.header('authorization');
     if (token !== null && token !== undefined && token !== '') {
