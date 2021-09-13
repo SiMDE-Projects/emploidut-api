56063cab04a1855fb7ef18feeb19284f91599331
Victor tondolo <victor.tondolo@gmail.com>
2021-09-13 12:37:27 +0200

    Enable find timeTable (emploidutemps) for one user
    Enable find timeTable (emploidutemps) for one user

--- src/repository/CourseRepository.ts
+++ src/repository/CourseRepository.ts
@@ -1,23 +1,32 @@
 import {EntityRepository, Repository} from "typeorm";
 import {Course} from "../entity/Course";
+import { User } from "../entity/User";
 
 @EntityRepository(Course)
 export class CourseRepository extends Repository<Course> {
-    findById(id: string){
+    findById(id: string) {
         return this.createQueryBuilder("course")
             .where("course.id = :id", { id})
             .getOne()
     }
 
-    findByName(name: string){
+    findByName(name: string) {
         return this.createQueryBuilder("course")
             .where("course.id = :id", { name})
             .getOne()
     }
 
-    findByCourseType(courseType: string){
+    findByCourseType(courseType: string) {
         return this.createQueryBuilder("course")
             .where("course.courseType = :id", { courseType})
+            .getMany()
+    }
+
+    findTimeTable(user: User) {
+        return this.createQueryBuilder("course")
+            .innerJoinAndSelect("course.timeslots", "ts")
+            .innerJoinAndSelect("ts.users", "u")
+            .where("u.id = :id", { id: user.id })
             .getMany()
     }
 }
